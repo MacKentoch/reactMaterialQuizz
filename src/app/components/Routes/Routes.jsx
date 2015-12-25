1f52dce1a77461a76825f4b4c8aa7b5a04620bbd
MacKentoch <erwan.datin@gmail.com>
2015-12-25 21:04:31 +0100

    chore : route clean

--- src/app/components/Routes/Routes.jsx
+++ src/app/components/Routes/Routes.jsx
@@ -26,16 +26,3 @@ export const Routes = ()=>{
   );
 }
 
-
-//html5 style but can't static website :
-// export const Routes = ()=>{
-//   return (
-//     <Router history={createBrowserHistory()}>
-//       <Route path='/' component={ReactMaterialQuizz}>
-//         <IndexRoute component={Home} />
-//         <Route path='about' component={About} />
-//         <Route path="quiz" component={Quiz} />
-//       </Route>
-//     </Router>
-//   );
-// }
