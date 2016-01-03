8d14fa8398dcd0595114fdafb8144b5fec96e089
MacKentoch <erwan.datin@gmail.com>
2016-01-03 06:57:53 +0100

    chore : .eslint file update

--- src/app/components/Home/Home.jsx
+++ src/app/components/Home/Home.jsx
@@ -3,13 +3,7 @@ import classNames               from 'classnames';
 import PromisedTimeout          from '../../services/PromisedTimeout.jsx!jsx';
 import MarginTop                from '../MarginTop/MarginTop.jsx!jsx';
 import MdlPaper                 from '../mdl/MdlPaper.jsx!jsx';
-
-import Paper                    from 'material-ui/lib/paper';
-import Card                     from 'material-ui/lib/card/card';
-import CardActions              from 'material-ui/lib/card/card-actions';
-import CardText                 from 'material-ui/lib/card/card-text';
-import CardTitle                from 'material-ui/lib/card/card-title';
-import {styles}                 from './home.style.jsx!jsx';
+//import {styles}                 from './home.style.jsx!jsx';
 
 export default class Home extends React.Component {
 
@@ -18,7 +12,7 @@ export default class Home extends React.Component {
     this.init();
   }
   
-  init(){
+  init() {
     this.state = {
       animated                    : true,
       viewEnters                  : false,
@@ -27,39 +21,36 @@ export default class Home extends React.Component {
     };
   }
   
-   
-  componentDidMount(){
+  componentWillMount() {
     this.setState({
       viewEnters                 : true,
       titleTwoAnimationActive    : false, 
       homeDetailsAnimationActive : false
-    });   
-    
-    
+    });     
+  }
+   
+  componentDidMount() {
   //// ANIMATE WITHOUT CLASSNAME - part1 -  : init (get node then init style obj)
   //   let titleTwo = this.refs.homeViewTitleTwo;    
   //   titleTwo.style.opacity            = 0;       
   //   titleTwo.style.animationDuration  = '0.3s';
   //   titleTwo.style.animationFillMode  = 'both';
-
-       
     let PromisedDelay = new PromisedTimeout();
     PromisedDelay
       .delay(400)
-      .then(()=>{
-        
+      .then(() => {
         //// ANIMATE WITHOUT CLASSNAME - part2 -  : set animation (manually set style object)
         // titleTwo.style.opacity = 1;
         // titleTwo.style.animation = 'fadeInUp 0.3s both ease-in';
         // titleTwo.style.zIndex = 9999;
         
         this.setState({
-          titleTwoAnimationActive : true
+          titleTwoAnimationActive: true
         });
         
         PromisedDelay
           .delay(800)
-          .then(()=>{
+          .then(() => {
             this.setState({
               homeDetailsAnimationActive : true
             });            
@@ -75,46 +66,42 @@ export default class Home extends React.Component {
     });     
   }
 
-  render(){
-    
+  render() {
     let homeViewClasses = classNames({
       'animatedViews'    : this.state.animated,
       'view-enter'       : this.state.viewEnters
-    });    
-    
+    });
     let homeTitle2Classes = classNames({
       'animated'    : this.state.animated,
       'invisible'   : !this.state.titleTwoAnimationActive,
-      'fadeInUp'    : this.state.titleTwoAnimationActive,
+      'fadeInUp'    : this.state.titleTwoAnimationActive
     });
-    
     let homeDetailsClasses = classNames({
       'animated'  : this.state.animated,
       'invisible' : !this.state.homeDetailsAnimationActive,
-      'zoomIn'    : this.state.homeDetailsAnimationActive,      
-    });    
-        
-    return (       
-      <section 
+      'zoomIn'    : this.state.homeDetailsAnimationActive
+    });
+    return (
+      <section
          key="homeView"
          className={homeViewClasses}>            
         <div className="mdl-grid">
           <div className="mdl-cell mdl-cell--12-col">
-            <MdlPaper>            
+            <MdlPaper>
               <h3>{this.context.translate.HOME_TITRE_1_QUIZZ}</h3>
-              <MarginTop 
+              <MarginTop
                 marginTopValue={80}
                 marginTopUnit={'px'} />
-              <h4 
+              <h4
                 ref="homeViewTitleTwo"
                 className={homeTitle2Classes}>{this.context.translate.HOME_TITRE_2_QUIZZ}</h4>
-              <p 
+              <p
                 ref="homeViewDetail"
                 className={homeDetailsClasses}>{this.context.translate.HOME_DETAIL_TEXT}</p>
-            </MdlPaper>          
+            </MdlPaper>
           </div>
         </div>
-      </section>   
+      </section>
     );
   }
 
@@ -124,4 +111,4 @@ export default class Home extends React.Component {
 Home.contextTypes = {
   muiTheme  : React.PropTypes.object,
   translate : React.PropTypes.object
-}
+};
